233298361ca452b13a9892eea3248797033467b8
Vladimir Katin <katin.v.v@gmail.com>
2023-04-20 13:22:04 +0300

    add a class WhisperTranscriber

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 import whisper
+from minio import Minio
+import io
 
-model = whisper.load_model("large")
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("/home/dzailz/Documents/russian.mp3")
-audio = whisper.pad_or_trim(audio)
+class WhisperTranscriber:
+    def __init__(self, model_name, minio_endpoint, minio_access_key, minio_secret_key, minio_bucket):
+        self.model = whisper.load_model(model_name)
+        self.minio_client = Minio(
+            minio_endpoint,
+            access_key=minio_access_key,
+            secret_key=minio_secret_key,
+        )
+        self.bucket = minio_bucket
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    def transcribe_audio(self, object_name):
+        # get log-Mel spectrogram from Minio
+        mel = self._get_log_mel_spectrogram(object_name)
+        # detect the spoken language
+        _, probs = self.model.detect_language(mel)
+        language = max(probs, key=probs.get)
+        # decode the audio
+        options = whisper.DecodingOptions(fp16=False)
+        result = whisper.decode(self.model, mel, options)
+        return language, result.text
 
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+    def _get_log_mel_spectrogram(self, object_name):
+        # get object from Minio
+        object_data = self.minio_client.get_object(self.bucket, object_name)
+        # read object data into memory
+        object_bytes = io.BytesIO(object_data.read())
+        # load audio and pad/trim it to fit 30 seconds
+        audio = whisper.load_audio(object_bytes)
+        audio = whisper.pad_or_trim(audio)
+        # make log-Mel spectrogram and move to the same device as the model
+        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
+        return mel
 
-# decode the audio
-options = whisper.DecodingOptions(fp16=False)
-result = whisper.decode(model, mel, options)
 
-# print the recognized text
-print(result.text)
+if __name__ == "__main__":
+    transcriber = WhisperTranscriber(
+        model_name="large",
+        minio_endpoint="your-minio-endpoint",
+        minio_access_key="your-access-key",
+        minio_secret_key="your-secret-key",
+        minio_bucket="your-bucket-name",
+    )
+
+    language, text = transcriber.transcribe_audio("your-audio-object-name")
+    print(f"Detected language: {language}")
+    print(f"Recognized text: {text}")
