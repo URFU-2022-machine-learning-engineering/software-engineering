603c63aad2c8d8249962a87e4ff8c3aca3158c89
Vladimir Katin <katin.v.v@gmail.com>
2023-10-08 22:26:22 +0300

    refactor transcribe handler

--- api/handlers/transcribe.py
+++ api/handlers/transcribe.py
@@ -1,23 +1,22 @@
 import logging
 import os
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Response, status
 
-from api.spec.transcribe import TranscribeRequest, TranscribeResponse
+from api.spec.transcribe import TranscribeRequest, TranscribeResponse, WhisperModels
 from connectors import remove_temp_file, save_temp_file
 from connectors.minio_connector import MinioClient
 from model import WhisperTranscriber
 
 router = APIRouter()
 
-model_name = "medium"
 minio_endpoint = os.getenv("MINIO_ENDPOINT")
 minio_access_key = os.getenv("MINIO_ACCESS_KEY")
 minio_secret_key = os.getenv("MINIO_SECRET_KEY")
 minio_use_ssl = bool(int(os.getenv("MINIO_USE_SSL"))) if os.getenv("MINIO_USE_SSL") else True
 
 
-logging.debug(f"minio_endpoint: {minio_endpoint}, use ssl is {minio_use_ssl}")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 @router.post(
@@ -25,25 +24,33 @@ logging.debug(f"minio_endpoint: {minio_endpoint}, use ssl is {minio_use_ssl}")
     response_model=TranscribeResponse,
     status_code=status.HTTP_200_OK,
 )
-def transcribe(req: TranscribeRequest) -> TranscribeResponse | None:
+def transcribe(req: TranscribeRequest, response: Response) -> TranscribeResponse | None:
+    audio_file = None
+    logging.debug(f"minio_endpoint: {minio_endpoint}, use ssl is {minio_use_ssl}")
     try:
         logging.debug(f"Received request {req}")
 
-        with MinioClient(
+        minio_client = MinioClient(
             minio_endpoint=minio_endpoint,
             minio_access_key=minio_access_key,
             minio_secret_key=minio_secret_key,
             minio_bucket=req.bucket,
             minio_use_ssl=minio_use_ssl,
-        ) as minio_client, WhisperTranscriber(model_name=model_name) as whisper:
-            audio_file = save_temp_file(minio_client.get_object(object_name=req.file_name))
-            language, text = whisper.transcribe_audio_file(audio_file_path=audio_file)
+        )
 
-            return TranscribeResponse(detected_language=language, recognized_text=text)
+        whisper = WhisperTranscriber(model_name=req.model if req.model else WhisperModels.medium)
+        audio_file = save_temp_file(minio_client.get_object(object_name=req.file_name))
+        language, text = whisper.transcribe_audio_file(audio_file_path=audio_file)
+
+        return TranscribeResponse(detected_language=language, recognized_text=text)
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
-        return TranscribeResponse(detected_language=None, recognized_text=None)
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return TranscribeResponse(detected_language="", recognized_text="")
     finally:
         if audio_file:
             remove_temp_file(audio_file)
+            logging.info(f"{audio_file} has been removed")
+        else:
+            logging.error("There is no saved audio_file")
