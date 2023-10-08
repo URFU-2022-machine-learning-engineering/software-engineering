4ab5c238230bd974b8df2ac29394787c02abd1ae
Vladimir Katin <katin.v.v@gmail.com>
2023-10-08 22:24:06 +0300

    add WhisperModels

--- api/spec/transcribe.py
+++ api/spec/transcribe.py
@@ -1,4 +1,20 @@
+from enum import StrEnum
+
 from pydantic import BaseModel
+
+
+class WhisperModels(StrEnum):
+    tiny_en = "tiny.en"
+    tiny = "tiny"
+    base_en = "base.en"
+    base = "base"
+    small_en = "small.en"
+    small = "small"
+    medium_en = "medium.en"
+    medium = "medium"
+    large_v1 = "large-v1"
+    large_v2 = "large-v2"
+    large = "large-v2"
 
 
 class TranscribeResponse(BaseModel):
@@ -7,5 +23,6 @@ class TranscribeResponse(BaseModel):
 
 
 class TranscribeRequest(BaseModel):
+    model: WhisperModels | None
     bucket: str
     file_name: str
