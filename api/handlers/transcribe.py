96e9fe4ec0db09b131f1c3d61616edb2e12a5dcb
Vladimir Katin <katin.v.v@gmail.com>
2023-10-23 22:04:46 +0300

    minor

--- api/handlers/transcribe.py
+++ api/handlers/transcribe.py
@@ -1,19 +1,14 @@
 import logging
-import os
 
 from fastapi import APIRouter, Response, status
 
+import settings
 from api.spec.transcribe import TranscribeRequest, TranscribeResponse, WhisperModels
 from connectors import remove_temp_file, save_temp_file
 from connectors.minio_connector import MinioClient
 from model import WhisperTranscriber
 
 router = APIRouter()
-
-minio_endpoint = os.getenv("MINIO_ENDPOINT")
-minio_access_key = os.getenv("MINIO_ACCESS_KEY")
-minio_secret_key = os.getenv("MINIO_SECRET_KEY")
-minio_use_ssl = bool(int(os.getenv("MINIO_USE_SSL"))) if os.getenv("MINIO_USE_SSL") else True
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -26,16 +21,15 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 )
 def transcribe(req: TranscribeRequest, response: Response) -> TranscribeResponse | None:
     audio_file = None
-    logging.debug(f"minio_endpoint: {minio_endpoint}, use ssl is {minio_use_ssl}")
+    logging.debug(f"minio_endpoint: {settings.MINIO_ENDPOINT}, use ssl is {settings.MINIO_USE_SSL}")
     try:
         logging.debug(f"Received request {req}")
-
         minio_client = MinioClient(
-            minio_endpoint=minio_endpoint,
-            minio_access_key=minio_access_key,
-            minio_secret_key=minio_secret_key,
+            minio_endpoint=settings.MINIO_ENDPOINT,
+            minio_access_key=settings.MINIO_ACCESS_KEY,
+            minio_secret_key=settings.MINIO_SECRET_KEY,
             minio_bucket=req.bucket,
-            minio_use_ssl=minio_use_ssl,
+            minio_use_ssl=settings.MINIO_USE_SSL,
         )
 
         whisper = WhisperTranscriber(model_name=req.model if req.model else WhisperModels.medium)
