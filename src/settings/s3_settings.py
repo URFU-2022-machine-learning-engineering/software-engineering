7cec4735b040c8ba1f55502ba1086705de515639
Vladimir Katin <katin.v.v@gmail.com>
2023-11-08 22:44:31 +0300

    minor

--- src/settings/s3_settings.py
+++ src/settings/s3_settings.py
@@ -1,13 +1,5 @@
 from decouple import config
 
-DB_HOST = config("DB_HOST", default="127.0.0.1", cast=str)
-DB_PORT = config("DB_PORT", default=5432, cast=int)
-DB_NAME = config("DB_NAME")
-DB_USER = config("DB_USER")
-DB_PASSWORD = config("DB_PASSWORD", cast=str)
-MIGRATIONS_VERSION = config("MIGRATIONS_VERSION")
-
-
 MINIO_ENDPOINT = config("MINIO_ENDPOINT")
 MINIO_ACCESS_KEY = config("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = config("MINIO_SECRET_KEY")
